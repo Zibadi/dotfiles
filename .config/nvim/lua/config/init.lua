4c2f0a314c1757c8d6b26f7da79ac2a372a67353
Zibadi <zybdym@gmail.com>
2025-09-24 18:41:27 +0330

    Change Alacritty font to Radon. Add neo-tree.lua. Add whichkey.lua. Add yazi. Add .tmux.conf

--- .config/nvim/lua/config/init.lua
+++ .config/nvim/lua/config/init.lua
@@ -1,20 +1,20 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 
 local opt = vim.opt
 opt.rtp:prepend(lazypath)
 
 -- Example using a list of specs with the default options
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.mapleader = " "       -- Make sure to set `mapleader` before lazy so your mappings are correct
 vim.g.maplocalleader = "\\" -- Same for `maplocalleader`
 
 opt.tabstop = 4
